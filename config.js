f1b996ced2abe67a1921f83829598c5d874f2788
Dagayya <151562869+Dagayya@users.noreply.github.com>
2024-02-07 00:16:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94742121040', 'me', true],
+  ['942121040', 'M.R ™Dagayya', true], 
   [''] 
 ] //Number of owners
 
